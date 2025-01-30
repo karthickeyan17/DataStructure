78dd8b9e0c3b8777f3d309751a117912119830e4
karthickeyan17 <skarthickeyan707@gmail.com>
2025-01-30 15:32:47 +0530

    added Deletion operation...

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -122,6 +122,44 @@ class BST:
         tem ="" if f(self.root) else "not "
         print(f"__ Value-{key} {tem}present in the tree __")
 
+    def min(self,root=None):
+        root = self.root if root==None else root
+        if not root:
+            return 
+        while root.left:
+            root = root.left
+        return root
+
+    def max(self,root=None):
+        root = self.root if root==None else root
+        if not root:
+            return
+        while root.right:
+            root=root.right
+        return root
+
+    def delete(self,key):
+        def dele(root,key):
+            if not root : return root
+
+            elif root.val>key :
+                root.left=dele(root.left,key)
+            elif root.val<key:
+                root.right=dele(root.right,key)
+            else:
+                if not root.right and not root.left : return None
+                elif root.left==None : return root.right
+                elif root.right==None : return root.left
+                else:
+                    tem = self.min(root.right)
+                    root.val=tem.val
+                    root.right = dele(root.right,root.val)
+            return root
+        if isinstance(key,int):
+            self.root = dele(self.root,key)
+        else:
+            for ele in key:
+                self.root = dele(self.root,ele)
 
         
 class CLI_BST:
@@ -129,10 +167,11 @@ class CLI_BST:
         self.tree = BST()
     def menu(self):
         print("\n\t|----- BST CLI-Application -----|\n")
-        print("1. Insert\t5. Height")
-        print("2. BulkInsert\t6. Search")
-        print("3. Traverse\t7. Find Min&Max values")
-        print("4. Reset\t8. Exit\n")
+        print("1. Insert\t6. Height")
+        print("2. BulkInsert\t7. Search")
+        print("3. Traverse\t8. Find Min&Max values")
+        print("4. Delete\t9. BulkDelete")
+        print("5. Reset\t10. Exit\n")
         self.tree.display()
     def run(self):
         while True:
@@ -153,22 +192,29 @@ class CLI_BST:
                     di = {1:"pre",2:"in",3:"post",4:"level"}
                     self.tree.traverse(di.get(ch))
                 elif opt==4:
+                    self.tree.delete(int(input("Enter value to delete : ")))
+                elif opt==5:
                     w = input("Are you sure to RESET (y/n):").strip().lower()=="y"
                     if w:
-                        self.tree.reset()
-                elif opt==5:
-                    self.tree.treeHeight()                
+                        self.tree.reset()              
                 elif opt==6:
-                    ele = int(input("Enter value to Search : "))
-                    self.tree.search(ele)
+                    self.tree.treeHeight()
                 elif opt==7:
-                    inor = self.tree.traverse(dis=False)
-                    if inor :
-                        print(f"MIN = {inor[0]} & MAX = {inor[-1]}")
+                    ele = int(input("Enter value to Search : "))
+                    self.tree.search(ele)               
                 elif opt==8:
+                    min_ = self.tree.min()
+                    if min_ :
+                        max_ = self.tree.max()
+                        print(f"MIN = {min_.val} & MAX = {max_.val}")
+                    else:
+                        print("Tree is empty!!!")
+                elif opt==9:
+                    self.tree.delete(list(map(int,input("Enter values to be deleted : ").split())))
+                elif opt==10:
                     print("\tBye Bye!!!!!!!!!!!!!!!")
                     print("\t\tYou Are Exiting CLI_APPLICATION--------->>>>>>")
-                    break               
+                    break
                 else:
                     print("--------->>Invalid Option !!!!\nTry Again")
 
